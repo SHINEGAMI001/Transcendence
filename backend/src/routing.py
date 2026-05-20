ef814332e08bbf0ae4afab7311c3485e611d32f5
shinegami001 <h11tamzwei@gmail.com>
2026-05-20 15:50:11 +0100

    fix missing slash

--- backend/src/routing.py
+++ backend/src/routing.py
@@ -2,7 +2,7 @@ from django.urls import path
 from chat.consumers import ChatConsumer, NotificationConsumer
 
 websocket_urlpatterns = [
-    path('ws/chat/<int:conversation_id>', ChatConsumer.as_asgi()),
+    path('ws/chat/<int:conversation_id>/', ChatConsumer.as_asgi()),
     path('ws/notifications/', NotificationConsumer.as_asgi())
 ]
 
